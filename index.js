7c18658a914d65136d8ec2d1798a27aa01fa47b6
Kigo-P <peterkigo33@gmail.com>
2024-06-28 08:28:45 +0300

    last commit

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Write your solution in this file!
+var customerName = "bob"
+
+function upperCaseCustomerName(){
+    customerName = customerName.toUpperCase()
+}
+
+
+
+function setBestCustomer(){ 
+    bestCustomer = "not bob"  
+    return bestCustomer
+}
+
+
+function overwriteBestCustomer(){
+    return bestCustomer = "maybe bob"
+}
+
+
+const leastFavoriteCustomer = "Zakayo"
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = "RiggyG"
+    return leastFavoriteCustomer
+}
